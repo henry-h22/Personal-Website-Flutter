8670274e86d10b19049fd0e491b46cbdca2d3498
Henry Heyden <henryden22@gmail.com>
2025-12-09 23:07:39 -0600

    flesh out bio card

--- lib/widgets/biocard.dart
+++ lib/widgets/biocard.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-const String bio = '   I am a senior Linguistics and Computer Science student at Macalester College. My research interests lie in the intersection of speech, machine learning, and music.';
+const String bio = '   I am a senior Linguistics and Computer Science student at Macalester College. My research interests lie in the intersection of speech, machine learning, and music. Outside of academics, I play jazz trombone and do audio engineering work in my school\'s concert hall.';
 
 Widget bioCard(BuildContext context) {
   return Row(children: <Widget>[Expanded(
     child: Container(
-      height: 120,
       padding: EdgeInsets.all(5.0),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.primaryContainer,
